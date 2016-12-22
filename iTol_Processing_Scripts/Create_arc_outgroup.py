13084fd013ab1745aea9712d6a41a8ca889f3eec
Erin Baggs <erin.baggs@earlham.ac.uk>
2016-12-22 14:38:18 +0000

    Initial commit, scripts from processing NLR-ID data for manuscript

---
+++ iTol_Processing_Scripts/Create_arc_outgroup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+import csv
+
+def iTOLcolours(fi):
+    not_seen = set()
+    #seen = set()
+"""
+colour = (',clade,#ff0000,normal,2')
+list = colour
+    
+fi=open(sys.argv[1])
+with open(sys.argv[1]) as fi:
+ for row in csv.reader(fi, delimiter=','):
+        print row + colour     
+"""
+
+fi=open(sys.argv[1])
+with open(sys.argv[1]) as fi:
+    #for line in fi:
+     # if not line.startswith('#'):
+      #    not_seen.add(line.strip())
+
+
+#unseenReads = iTOLcolours(sys.stdin)
+
+    for line in fi:
+        print (line.strip() +',#0000ff,OUTGROUP')
+
+        
+"""
+        if not row or row[0].startswith('#'):
+              
+           continue
+        #print (row) 
+        #elif re.search(',1',row[0]): #pattern, where to search
+        if '1' in row:    
+            #_id = ','.join(row[0].split(',')[1:])
+            not_seen.add(row[0])  
+           # print ('added')
+    return not_seen    
+            
+
+unseenReads = iTOLcolours(sys.stdin)
+
+for row in (unseenReads):
+    print (row +',clade,#ff0000,normal,2')
+
+# csv files no empty lines expected 
+# if not row (means if a row is empty) or (logical)    
+"""
---
+++ iTol_Processing_Scripts/sector_colour_hotspot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+import csv
+
+def iTOLcolours(fi):
+    not_seen = set()
+    seen = set()
+    for row in csv.reader(fi, delimiter=','):
+        
+        if not row or row[0].startswith('#'):
+              
+           continue
+        #print (row) 
+        #elif re.search(',1',row[0]): #pattern, where to search
+        if '.' or ' ' or '[A-Z]' in row :     
+            #_id = ','.join(row[0].split(',')[1:])
+            not_seen.add(row[0])  
+           # print ('added')
+    return not_seen    
+            
+
+unseenReads = iTOLcolours(sys.stdin)
+
+for row in (unseenReads):
+    print (row +',range,#ff0000,normal,2')
+
+# csv files no empty lines expected 
+# if not row (means if a row is empty) or (logical)    
